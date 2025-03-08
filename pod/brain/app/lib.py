47357a4d58ed1ab6346c2fc28c04ca6855b11837
Uli <sorjak1@gmail.com>
2025-03-08 12:55:24 -0800

    changes to pinouts + allow setting multiple pins at once

--- pod/brain/app/lib.py
+++ pod/brain/app/lib.py
@@ -6,22 +6,38 @@ GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 
 scenes = {
-    'angry': 23, 
-    'happy': 24, 
-    'stasis': 22, 
-    'idle': 27
+    'angry': 22, 
+    'happy': 27, 
+    'stasis': 17, 
+    'idle': 4
 }
 
 def init_board():
     for pin in scenes.values():
         GPIO.setup(pin, GPIO.OUT)
 
-
-def set_scene(scene):
+def get_scene_pin(scene: str) -> int:
     scene_pin = int(scenes.get(scene, -1))
     if scene_pin < 0:
         raise Exception(f'Invalid scene provided: {scene}')
+    
+    return scene_pin
 
+def set_scene(scene: str):
+    scene_pin = get_scene_pin(scene)
     GPIO.output(scene_pin, GPIO.HIGH)
     time.sleep(1)
     GPIO.output(scene_pin, GPIO.LOW)
+
+def set_scenes(scenes: list[str]):
+    pins = []
+    for scene in scenes:
+        pins.append(get_scene_pin(scene))
+
+    for pin in pins:
+        GPIO.output(pin, GPIO.HIGH)
+    
+    time.sleep(1)
+
+    for pin in pins:
+        GPIO.output(pin, GPIO.LOW)
--- pod/brain/main.py
+++ pod/brain/main.py
@@ -2,20 +2,29 @@ import time
 
 import click
 
-from app.lib import GPIO, set_scene
+from app.lib import set_scene, set_scenes
 
 @click.group()
 def cli():
     pass
 
 @cli.command()
-@click.argument('mood')
-def set_mood(mood):
+@click.argument('scene')
+def set_scene(scene):
     try:
-        set_scene(mood)
+        set_scene(scene)
     except Exception as e:
         print(e)
 
+
+@cli.command()
+@click.argument('scenes', nargs=-1)
+def set_scenes(scenes):
+    print(f'Setting scenes: {scenes}')
+    try:
+        set_scenes(scenes)
+    except Exception as e:
+        print(e)
 
 if __name__ == '__main__':
     cli()
