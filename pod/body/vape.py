235a172062d8e4b18701ea88dd1b5be00642e095
Uli <sorjak1@gmail.com>
2025-04-04 00:11:42 -0700

    tinkering with vape scorpios

--- pod/body/vape.py
+++ pod/body/vape.py
@@ -9,7 +9,7 @@ import neopixel
 first_input = digitalio.DigitalInOut(board.A0)
 second_input = digitalio.DigitalInOut(board.A1)
 third_input = digitalio.DigitalInOut(board.A2)
-inputs = [first_input, second_input, third_input]
+inputs = [third_input, second_input, first_input]
 
 vape_0 = pwmio.PWMOut(board.NEOPIXEL0)
 vape_1 = pwmio.PWMOut(board.NEOPIXEL1)
@@ -36,7 +36,7 @@ scenes = {
 }
 
 color_ref = {
-    "none": (0, 0, 0),          # 000
+    "none":   (0, 0, 0),        # 000
     "scene1": (0, 0, 255),      # 001
     "scene2": (0, 255, 0),      # 010
     "scene3": (0, 255, 255),    # 011
@@ -49,35 +49,36 @@ color_ref = {
 def get_input_values() -> list[bool]:
     return [x.value for x in inputs]
 
-def get_scene_from_input_values(values: list[bool]) -> str:
-    scene_val = 0
+def get_input_value(values: list[bool]) -> int:
+    val = 0
     for idx, value in enumerate(values):
         if value:
-            scene_val = scene_val ^ (2 ** idx)
+            val = val ^ (2 ** idx)
 
-    return scenes.get(scene_val, "none")
+    return val
 
 
-print(f'starting loop')
+print(f'starting vape program')
 loop_counter = 0
 scene = 'none'
+old_scene = 'none'
 while True:
     board_pixel[0] = (0, 0, 0)
     values = get_input_values()
     if any(values):
-        print(values)
+        input_val = get_input_value(values)
+        print(f'Values {values} -> decimal {input_val}')
 
-    scene = get_scene_from_input_values(values)
-    
-    if scene != 'none':
+        scene = scenes.get(input_val)
         print(f'changing to scene {scene}')
+
         color = color_ref.get(scene)
         board_pixel[0] = color
+        old_scene = scene
         
     board_pixel.show()
     loop_counter += 1
     loop_counter = loop_counter % 1000
 
     if loop_counter == 0:
-        print(f'new loop, current scene: {scene}')
-
+        print(f'new loop, last scene was: {old_scene}')
