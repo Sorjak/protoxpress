7819098f6e431af6970c40a7fe45299131aeb643
Uli <sorjak1@gmail.com>
2025-04-04 21:02:57 -0700

    vapes working code

--- pod/body/vape.py
+++ pod/body/vape.py
@@ -17,6 +17,7 @@ vape_2 = pwmio.PWMOut(board.NEOPIXEL2)
 vape_3 = pwmio.PWMOut(board.NEOPIXEL3)
 vape_4 = pwmio.PWMOut(board.NEOPIXEL4)
 vape_5 = pwmio.PWMOut(board.NEOPIXEL5)
+all_vapes = [vape_0, vape_1, vape_2, vape_3, vape_4, vape_5]
 
 for ipt in inputs:
     ipt.direction = digitalio.Direction.INPUT
@@ -24,26 +25,28 @@ for ipt in inputs:
 
 board_pixel = neopixel.NeoPixel(board.NEOPIXEL, 1, brightness=1.0)
 
+max_vape_power = 2**16 - 1
+
 scenes = {
     0: "none",
-    1: "scene1",
-    2: "scene2",
-    3: "scene3",
-    4: "scene4",
-    5: "scene5",
-    6: "scene6",
-    7: "scene7"
+    1: "abort",
+    2: "test",
+    3: "init",
+    4: "happy",
+    5: "sad",
+    6: "emergency",
+    7: "photo"
 }
 
 color_ref = {
     "none":   (0, 0, 0),        # 000
-    "scene1": (0, 0, 255),      # 001
-    "scene2": (0, 255, 0),      # 010
-    "scene3": (0, 255, 255),    # 011
-    "scene4": (255, 0, 0),      # 100
-    "scene5": (255, 0, 255),    # 101
-    "scene6": (255, 255, 0),    # 110
-    "scene7": (255, 255, 255)   # 111
+    "abort": (0, 0, 255),      # 001
+    "test": (0, 255, 0),      # 010
+    "init": (0, 255, 255),    # 011
+    "happy": (255, 0, 0),      # 100
+    "sad": (255, 0, 255),    # 101
+    "emergency": (255, 255, 0),    # 110
+    "photo": (255, 255, 255)   # 111
 }
 
 def get_input_values() -> list[bool]:
@@ -58,8 +61,22 @@ def get_input_value(values: list[bool]) -> int:
     return val
 
 
+def handle_vapes(scene, counter):
+    for v in all_vapes:
+        if counter < 500:
+            turn_vape_on(v)
+        else:
+            turn_vape_off(v)
+
+def turn_vape_on(vape):
+    vape.duty_cycle = max_vape_power
+
+def turn_vape_off(vape):
+    vape.duty_cycle = 0
+
 print(f'starting vape program')
 loop_counter = 0
+vape_counter = 0
 scene = 'none'
 old_scene = 'none'
 while True:
@@ -67,7 +84,7 @@ while True:
     values = get_input_values()
     if any(values):
         input_val = get_input_value(values)
-        print(f'Values {values} -> decimal {input_val}')
+        # print(f'Values {values} -> decimal {input_val}')
 
         scene = scenes.get(input_val)
         print(f'changing to scene {scene}')
@@ -79,6 +96,9 @@ while True:
     board_pixel.show()
     loop_counter += 1
     loop_counter = loop_counter % 1000
+
+    if scene != 'none':
+        handle_vapes(scene, loop_counter)
 
     if loop_counter == 0:
         print(f'new loop, last scene was: {old_scene}')
